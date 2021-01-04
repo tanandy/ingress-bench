28b5f1abcda28122cf015df02397e981caa38586
Andy TAN <atan@linagora.com>
2021-01-04 11:44:42 +0100

    Update tests

--- gatling/src/test/scala/helloapp/IngressSimulationLoadTest500Users.scala
+++ gatling/src/test/scala/helloapp/IngressSimulationLoadTest500Users.scala
@@ -4,7 +4,7 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class IngressSimulationLoadTest5000Users extends Simulation {
+class IngressSimulationLoadTest500Users extends Simulation {
   val ingressBaseUrl = System.getenv("INGRESS_BASE_URL")
 
   val httpProtocol = http
@@ -14,9 +14,9 @@ class IngressSimulationLoadTest5000Users extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Load Scenario") // Simulating an expected load of 5000 concurrent users
+  val scn = scenario("Load Scenario") // Simulating an expected load of 500 concurrent users
     .exec(http("Access Home Page")
       .get("/"))
  
-  setUp(scn.inject(atOnceUsers(5000))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(500))).protocols(httpProtocol)
 }
